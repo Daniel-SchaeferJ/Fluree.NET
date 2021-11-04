0a9abbbf425e37323eb62ccd731c89e64a0505d0
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-11-03 22:31:15 -0700

    Added appsettings and folders

---
+++ src/FlureeDotnetLibrary/FlureeQuery/FLureeQuery.cs
@@ -0,0 +1,29 @@
+﻿using Flurl.Http;
+using Flurl.Http.Configuration;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlureeDotnetLibrary.FlureeQuery
+{
+    public interface IExecuteSingleFlureeQuery
+    {
+
+    }
+    public class FLureeQuery
+    {
+        private readonly IFlurlClient _flurlClient;
+
+        public FLureeQuery(IFlurlClientFactory factory, IConfiguration config)
+        {
+            _flurlClient = factory.Get(config["fluree"]);
+        }
+
+        public async Task ExectureSingleFlureeQuery()
+        {
+
+        }
+    }
+}
